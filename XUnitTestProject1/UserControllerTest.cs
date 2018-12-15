92277262f615787a100674b2e5ec2f063f36d6ec
我性别男爱好女 <543539501@qq.com>
2018-12-15 23:50:15 +0800

    添加单元测试

--- XUnitTestProject1/UserControllerTest.cs
+++ XUnitTestProject1/UserControllerTest.cs
@@ -1,19 +1,27 @@
 using FirstProject.Controllers;
 using System;
 using FirstProject.Data;
+using FirstProject.Model;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace XUnitTestProject1
 {
     public class UserControllerTest
     {
-        //private UserContext GetUserContext()
-        //{
+        private UserContext GetUserContext()
+        {
+            var option = new DbContextOptionsBuilder<UserContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            var userContext = new UserContext(option);
 
-        //}
+            userContext.AppUsers.Add(new AppUser() { Id = 1, Name = "quliting" });
+            userContext.SaveChanges();
+            return userContext;
+        }
         [Fact]
         public void Test1()
         {
+           var context= GetUserContext();
             //var controller=new UserController();
         }
     }
